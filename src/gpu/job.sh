daee655021247c0d3e5fcff74f8825d105d6c06c
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2023-12-20 18:43:44 +0100

    Test GPU

--- src/gpu/job.sh
+++ src/gpu/job.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=edu5
 #SBATCH --nodes=1
 #SBATCH --tasks=1
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:05:00
+#SBATCH --time=00:00:30
 #SBATCH --job-name=GPU_SAT
 #SBATCH --output=out/out.txt
 #SBATCH --error=out/err.txt
